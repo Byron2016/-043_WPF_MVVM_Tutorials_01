456ad29dd064aeb2ec90ef60888c1af8d3bd7050
Byron 2016 <bgva2005@yahoo.com>
2022-03-15 13:56:06 -0500

    feat: 003 add ColorChatColorViewModel

---
+++ aac_ColorChat.WPF/ViewModels/ColorChatColorViewModel.cs
@@ -0,0 +1,38 @@
+﻿using aac_ColorChat.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace aac_ColorChat.WPF.ViewModels
+{
+    public class ColorChatColorViewModel : ViewModelBase
+    {
+        public ColorChatColor ColorChatColor { get; set; }
+
+        public Brush ColorBrush
+        {
+            get
+            {
+                try
+                {
+                    return new SolidColorBrush(Color.FromRgb(
+                        ColorChatColor.Red,
+                        ColorChatColor.Green,
+                        ColorChatColor.Blue));
+                }
+                catch (FormatException)
+                {
+                    return new SolidColorBrush(Colors.Black);
+                }
+            }
+        }
+
+        public ColorChatColorViewModel(ColorChatColor colorChatColor)
+        {
+            ColorChatColor = colorChatColor;
+        }
+    }
+}
