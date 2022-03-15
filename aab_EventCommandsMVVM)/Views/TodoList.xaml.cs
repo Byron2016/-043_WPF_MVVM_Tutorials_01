6103003aa5f804dabd817cc426ef1b629e4d161f
Byron 2016 <bgva2005@yahoo.com>
2022-03-15 08:26:24 -0500

    feat: 002 IE estado inicial prerrequisitos

--- aab_EventCommandsMVVM)/Views/TodoList.xaml.cs
+++ aab_EventCommandsMVVM)/Views/TodoList.xaml.cs
@@ -1,5 +1,4 @@
-﻿using aab_EventCommandsMVVM_.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,18 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace aab_EventCommandsMVVM_
+namespace aab_EventCommandsMVVM_.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TodoList.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TodoList : UserControl
     {
-        public MainWindow()
+        public TodoList()
         {
             InitializeComponent();
-
-            DataContext = new MainViewModel();
         }
     }
 }
