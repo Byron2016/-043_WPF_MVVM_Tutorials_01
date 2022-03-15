ae92c3363190f9ae6971101105ebc9522545170f
Byron 2016 <bgva2005@yahoo.com>
2022-03-15 09:07:55 -0500

    feat: 002 IE add command prop to TodoListViewModel

--- aab_EventCommandsMVVM)/ViewModels/TodoListViewModel.cs
+++ aab_EventCommandsMVVM)/ViewModels/TodoListViewModel.cs
@@ -1,10 +1,12 @@
-﻿using aab_EventCommandsMVVM_.Models;
+﻿using aab_EventCommandsMVVM_.Commands;
+using aab_EventCommandsMVVM_.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace aab_EventCommandsMVVM_.ViewModels
 {
@@ -22,6 +24,13 @@ namespace aab_EventCommandsMVVM_.ViewModels
 				_todoItems = value;
 				OnPropertyChanged(nameof(TodoItems));
 			}
+		}
+
+		public ICommand LoadTodoItemsCommand { get; set; }
+
+		public TodoListViewModel()
+		{
+			LoadTodoItemsCommand = new LoadTodoItemsCommand(this);
 		}
 	}
 }
