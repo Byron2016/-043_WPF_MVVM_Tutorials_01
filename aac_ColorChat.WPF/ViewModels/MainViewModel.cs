65ef9902d93f323ab349f04dbe14a76b9d3aae48
Byron 2016 <bgva2005@yahoo.com>
2022-03-15 14:03:01 -0500

    feat: 003 add MainViewModel

---
+++ aac_ColorChat.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aac_ColorChat.WPF.ViewModels
+{
+    public class MainViewModel
+    {
+        public ColorChatViewModel ColorChatViewModel { get; }
+
+        public MainViewModel(ColorChatViewModel chatViewModel)
+        {
+            ColorChatViewModel = chatViewModel;
+        }
+    }
+}
