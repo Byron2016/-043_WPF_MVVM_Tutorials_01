d601c4d92eeec49e0939c01c59b97ec5f614ffb3
Byron 2016 <bgva2005@yahoo.com>
2022-03-15 08:30:16 -0500

    feat: 002 IE add MainViewModels

---
+++ aab_EventCommandsMVVM)/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using aab_EventCommandsMVVM_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aab_EventCommandsMVVM_.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+    }
+}
