e316d513d8cf348bb04e59c986129e9f14e86b87
Byron 2016 <bgva2005@yahoo.com>
2022-03-15 14:22:39 -0500

    feat: 003 add code begind to component ColorPicker.xaml

--- aac_ColorChat.WPF/Components/ColorPicker.xaml.cs
+++ aac_ColorChat.WPF/Components/ColorPicker.xaml.cs
@@ -20,9 +20,61 @@ namespace aac_ColorChat.WPF.Components
     /// </summary>
     public partial class ColorPicker : UserControl
     {
+        public static readonly DependencyProperty RedProperty =
+           DependencyProperty.Register(nameof(Red), typeof(byte), typeof(ColorPicker),
+               new FrameworkPropertyMetadata((byte)0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, RGBPropertyChanged));
+
+        public static readonly DependencyProperty GreenProperty =
+            DependencyProperty.Register(nameof(Green), typeof(byte), typeof(ColorPicker),
+               new FrameworkPropertyMetadata((byte)0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, RGBPropertyChanged));
+
+        public static readonly DependencyProperty BlueProperty =
+            DependencyProperty.Register(nameof(Blue), typeof(byte), typeof(ColorPicker),
+               new FrameworkPropertyMetadata((byte)0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, RGBPropertyChanged));
+
+        public static readonly DependencyProperty ColorBrushProperty =
+           DependencyProperty.Register(nameof(ColorBrush), typeof(Brush), typeof(ColorPicker),
+               new FrameworkPropertyMetadata(new SolidColorBrush(Colors.Black)));
+
+        public byte Red
+        {
+            get { return (byte)GetValue(RedProperty); }
+            set { SetValue(RedProperty, value); }
+        }
+        public byte Green
+        {
+            get { return (byte)GetValue(GreenProperty); }
+            set { SetValue(GreenProperty, value); }
+        }
+
+        public byte Blue
+        {
+            get { return (byte)GetValue(BlueProperty); }
+            set { SetValue(BlueProperty, value); }
+        }
+
+        public Brush ColorBrush
+        {
+            get { return (Brush)GetValue(ColorBrushProperty); }
+            set { SetValue(ColorBrushProperty, value); }
+        }
+
         public ColorPicker()
         {
             InitializeComponent();
+        }
+
+        private static void RGBPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ColorPicker colorPicker)
+            {
+                colorPicker.UpdateColorBrush();
+            }
+        }
+
+        private void UpdateColorBrush()
+        {
+            ColorBrush = new SolidColorBrush(Color.FromRgb(Red, Green, Blue));
         }
     }
 }
