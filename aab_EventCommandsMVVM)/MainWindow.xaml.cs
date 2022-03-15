d516761a8891593f565258ce99e97599a2a70375
Byron 2016 <bgva2005@yahoo.com>
2022-03-15 08:44:03 -0500

    feat: 002 IE add DataContext to MainVwindow.xaml.cs

--- aab_EventCommandsMVVM)/MainWindow.xaml.cs
+++ aab_EventCommandsMVVM)/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using aab_EventCommandsMVVM_.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace aab_EventCommandsMVVM_
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainViewModel();
         }
     }
 }
