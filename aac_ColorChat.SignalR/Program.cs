5c9ece3835bb1f8d4b08c2b228a00e96531f21a4
Byron 2016 <bgva2005@yahoo.com>
2022-03-16 09:18:04 -0500

    feat: 003 SIGNALR register hub as a end point

--- aac_ColorChat.SignalR/Program.cs
+++ aac_ColorChat.SignalR/Program.cs
@@ -1,3 +1,5 @@
+using aac_ColorChat.SignalR.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSignalR();
@@ -5,5 +7,6 @@ builder.Services.AddSignalR();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapHub<ColorChatHub>("/colorchat");
 
 app.Run();
