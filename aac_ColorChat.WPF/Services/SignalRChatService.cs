9602d30542a7254bdfde0dc02b2b5d0589ac4331
Byron 2016 <bgva2005@yahoo.com>
2022-03-16 09:49:56 -0500

    feat: 003 SilnalR service enable hub conection into service

--- aac_ColorChat.WPF/Services/SignalRChatService.cs
+++ aac_ColorChat.WPF/Services/SignalRChatService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace aac_ColorChat.WPF.Services
 {
     public class SignalRChatService
     {
+        private readonly HubConnection _connection;
+
+        public SignalRChatService(HubConnection connection)
+        {
+            _connection = connection;
+        }
     }
 }
