4677069f434b57194ebdc375e10b08708b19d5b7
Byron 2016 <bgva2005@yahoo.com>
2022-03-16 19:33:12 -0500

    feat: 004 IE move MainWindow startup to App.xaml.cs

--- aad_AsyncCommands/App.xaml.cs
+++ aad_AsyncCommands/App.xaml.cs
@@ -13,5 +13,13 @@ namespace aad_AsyncCommands
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
