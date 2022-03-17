ac667996fc84af4f8524e4161b45ffd242e3d6f1
Byron 2016 <bgva2005@yahoo.com>
2022-03-16 21:48:16 -0500

    feat: 004 F2 add AsyncRelayCommand 10.46

---
+++ aad_AsyncCommands/Commands/AsyncRelayCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aad_AsyncCommands.Commands
+{
+    public class AsyncRelayCommand : AsyncCommandBase
+    {
+        public AsyncRelayCommand(Action<Exception> onException) : base(onException)
+        {
+        }
+
+        protected override Task ExecuteAsync(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
