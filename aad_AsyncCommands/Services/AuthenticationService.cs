f29a9d53fd716b3f194dcb96b1525da0a18e0b6d
Byron 2016 <bgva2005@yahoo.com>
2022-03-16 20:24:17 -0500

    feat: 004 IE add AuthenticationService

---
+++ aad_AsyncCommands/Services/AuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aad_AsyncCommands.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public async Task Login(string username)
+        {
+            await Task.Delay(5000);
+        }
+    }
+}
