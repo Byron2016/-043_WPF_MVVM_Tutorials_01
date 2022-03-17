d918ab21ec3569e295b30451e83a5b0a00f9cb45
Byron 2016 <bgva2005@yahoo.com>
2022-03-16 20:44:24 -0500

    feat: 004 F1 add LoginCommand

---
+++ aad_AsyncCommands/Commands/LoginCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aad_AsyncCommands.Commands
+{
+    public class LoginCommand : AsyncCommandBase
+    {
+        protected override Task ExecuteAsync(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
