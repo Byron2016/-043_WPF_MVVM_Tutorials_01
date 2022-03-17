03c25542629520b78aabf5f36a15f31652d58969
Byron 2016 <bgva2005@yahoo.com>
2022-03-16 19:57:15 -0500

    feat: 004 IE add LoginViewModel

---
+++ aad_AsyncCommands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace aad_AsyncCommands.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _username;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        private string _statusMessage;
+        public string StatusMessage
+        {
+            get
+            {
+                return _statusMessage;
+            }
+            set
+            {
+                _statusMessage = value;
+                OnPropertyChanged(nameof(StatusMessage));
+                OnPropertyChanged(nameof(HasStatusMessage));
+            }
+        }
+
+        public bool HasStatusMessage => !string.IsNullOrEmpty(StatusMessage);
+
+        public ICommand LoginCommand { get; }
+
+        
+    }
+}
