afc21f0edb3bcfb08fbd9da51fc6656008597746
Byron 2016 <bgva2005@yahoo.com>
2022-04-01 14:54:26 -0500

    feat: 005 IE Starting point

--- aae_LayoutComponents_StylesApproach/MainWindow.xaml.cs
+++ aae_LayoutComponents_StylesApproach/MainWindow.xaml.cs
@@ -1,28 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using aae_LayoutComponents_StylesApproach.Views;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace aae_LayoutComponents_StylesApproach
 {
+    public enum ViewType
+    {
+        Home,
+        About
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ViewType _viewType;
+        private ViewType ViewType
+        {
+            get
+            {
+                return _viewType;
+            }
+            set
+            {
+                _viewType = value;
+                UpdateView();
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+
+            ViewType = ViewType.Home;
+        }
+
+        private void NavHome_Click(object sender, RoutedEventArgs e)
+        {
+            ViewType = ViewType.Home;
+        }
+
+        private void NavAbout_Click(object sender, RoutedEventArgs e)
+        {
+            ViewType = ViewType.About;
+        }
+
+        private void UpdateView()
+        {
+            switch (ViewType)
+            {
+                case ViewType.Home:
+                    _root.Content = new HomeView();
+                    break;
+                case ViewType.About:
+                    _root.Content = new AboutView();
+                    break;
+            }
         }
     }
 }
