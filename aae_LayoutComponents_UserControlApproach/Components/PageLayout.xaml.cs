fc8b5d3607eecfff026cb8d8d3aa3028699782e7
Byron 2016 <bgva2005@yahoo.com>
2022-04-01 16:12:56 -0500

    feat: 005_01 UserControl passing Header's text as parameter

--- aae_LayoutComponents_UserControlApproach/Components/PageLayout.xaml.cs
+++ aae_LayoutComponents_UserControlApproach/Components/PageLayout.xaml.cs
@@ -20,6 +20,15 @@ namespace aae_LayoutComponents_UserControlApproach.Components
     /// </summary>
     public partial class PageLayout : UserControl
     {
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(string), typeof(PageLayout), new PropertyMetadata(string.Empty));
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
         public PageLayout()
         {
             InitializeComponent();
